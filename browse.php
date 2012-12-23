9bea54029155b1e2d97d8e016323e1daac419171
Nick Porillo <milkywayz@mail.com>
2012-12-23 13:14:43 -0500

    Browse page placeholder to avoid 404

---
+++ browse.php
@@ -0,0 +1,21 @@
+<?php require("functions.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MineAuthor</title>
+	<?php headIncludes(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div class="container">
+
+      <div class="content">
+        <div class="page-header">
+          <h1>MineWriter <small>Author downloadable books for minecraft!</small></h1>
+        </div> 
+        	Browse page placeholder     
+            </div>
+      </div>
+  </body>
+</html>
