b373dee81c07a6f04bc0994224993dd455956d9f
lol768 <support@technostripe.com>
2012-12-23 18:28:46 +0000

    404 page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php require("functions.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MineWriter</title>
+	<?php headIncludes(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div class="container">
+
+      <div class="content">
+        <div class="page-header">
+          <h1>404 <small>We couldn't find that page :(</small></h1>
+        </div>      
+        <p>Sorry, but that page could not be found on the server</p>
+            </div>
+      </div>
+  </body>
+</html>
