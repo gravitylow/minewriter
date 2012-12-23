00eefa978d97f09469777eb44d24ebef1dd29649
lol768 <support@technostripe.com>
2012-12-23 19:28:50 +0000

    Link spacing

--- team.php
+++ team.php
@@ -19,9 +19,9 @@
               	<h2 class="big">Meet the MineWriter Developer Team / Contributors</h2>   
               	<ul class = "team">
               	<!--Syntax=Name with link to github - Persons job / place-->
-              	<li class="big team"><a href="https://github.com/milkywayz"><img src = "http://forums.bukkit.org/data/avatars/s/90595/90595764.jpg?1351914681" /> Milkywayz</a> - Project lead</li>
-              	<li class="big team"><a href="https://github.com/lol768"><img src = "http://forums.bukkit.org/data/avatars/s/90686/90686461.jpg?1354215989" /> Lol768</a> - Project Developer </li>
-              	<li class="big team"><a href="https://github.com/gomeow"><img src = "http://forums.bukkit.org/data/avatars/s/90728/90728305.jpg?1354849565" /> Gomeow</a> - Project Contributor</li>
+              	<li class="big team"><a href="https://github.com/milkywayz"><img src = "http://forums.bukkit.org/data/avatars/s/90595/90595764.jpg?1351914681" /></a> <a href="https://github.com/milkywayz">Milkywayz</a> - Project lead</li>
+              	<li class="big team"><a href="https://github.com/lol768"><img src = "http://forums.bukkit.org/data/avatars/s/90686/90686461.jpg?1354215989" /></a> <a href="https://github.com/lol768">Lol768</a> - Project Developer </li>
+              	<li class="big team"><a href="https://github.com/gomeow"><img src = "http://forums.bukkit.org/data/avatars/s/90728/90728305.jpg?1354849565" /></a> <a href="https://github.com/gomeow">Gomeow</a> - Project Contributor</li>
               	</ul>              
                </div>
             </div>
