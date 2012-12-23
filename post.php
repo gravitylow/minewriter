eb20da3753af7481de93a437070947f5c07867fb
lol768 <support@technostripe.com>
2012-12-23 15:20:12 +0000

    Add some safeguards and ensure a unique id
    Rand can cause collisions.

--- post.php
+++ post.php
@@ -1,10 +1,13 @@
-
 <?php
 $Author=$_POST["author"];
 $Title =$_POST["title"];
 $Content=$_POST["content"];
-$file="/books/" . rand().".txt";
+$file="/books/" . uniqid().".txt";
 //This is where we format the entire book
+//Sanatise the author and title variables
+//This allows alphanumeric, underscores and some punctuation, stripping out new lines etc
+$Title = preg_replace('/[^a-zA-Z0-9_ \\\.\(\)-\]/s', '', $Title);
+$Author = preg_replace('/#[^a-z0-9_.-]#i/', '', str_replace(".", "", $author));
 $book = "!Author-" . $Author . "\n" . "!Title-" . $Title . "\n" . $Content;
 file_put_contents($file, $book);
 ?>
