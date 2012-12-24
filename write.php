758c92cb9feda921c11a41a3de902ce448feee99
lol768 <support@technostripe.com>
2012-12-24 14:35:02 +0000

    Styling of book textbox

--- write.php
+++ write.php
@@ -15,7 +15,7 @@
           <h1>Write <small>Submit your book here!</small></h1>
         </div> 
         <form name="myform" action="post.php" method="POST">
-       <textarea style = "width: 600px; height: 740px; border: 0; background-image: url('img/book.png');" onKeyPress="update(this.value);" onkeydown="update(this.value);"></textarea>
+       <textarea class = "book" style = "width: 600px; height: 740px; border: 0; background-image: url('img/book.png'); padding: 0!important;" onKeyPress="update(this.value);" onkeydown="update(this.value);"></textarea>
             </div>
         </form>
       </div>
