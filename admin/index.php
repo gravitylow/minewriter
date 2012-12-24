ec6952f317572c321958a180c9502e3a6632a3ab
Nick Porillo <milkywayz@mail.com>
2012-12-24 01:01:10 -0500

    Use bootstrap forms

--- admin/index.php
+++ admin/index.php
@@ -13,15 +13,16 @@
       <div class="content">
         <div class="page-header">
           <h1>Admins <small>Admin Control Panel</small></h1>
-        </div class="admin"> 
-        	    Login<br /><br />
-        	    <form action="adminVerify.php" method="POST">
-        	    	UserName:<br />
-        	    	<input type="text"/><br />
-        	    	PassWord:</br />
-        	    	<input type="password" />
+          </div class="admin"> 
+        	    <form class="form-inline">
+        	    <input type="text" class="input-small" placeholder="Username" name="user">
+        	    <input type="password" class="input-small" placeholder="Password" name="pass">
+        	    <label class="checkbox">
+        	    <input type="checkbox"> Remember me
+        	    </label>
+        	    <button type="submit" class="btn">Sign in</button>
         	    </form>
-        </div>
+      </div>
       </div>
   </body>
 </html>
