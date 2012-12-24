d2ec81c98df36f1c04726fdb8c9afee2cab48fd2
Nick Porillo <milkywayz@mail.com>
2012-12-24 02:33:31 -0500

    Ditch functions they never work :/

--- query.php
+++ query.php
@@ -8,7 +8,6 @@ $Ip = $_SERVER['REMOTE_ADDR'];
 
 $link = mysql_connect($dbHost, $dbUser, $dbPass) or die('Connection to MySQL has failed!'); 
 if(is_null($id)) {
-	//No ID, lets check if theres anything else
 	if(is_null($Author)) {
 		die("Query is missing the author");
 	} else if(is_null($Title)) {
@@ -16,25 +15,36 @@ if(is_null($id)) {
 	} else if(is_null($Type)) {
 		die("Query is missing the type");
 	}	
-	doDbWork();
+	mysql_select_db($dbName) or die("Could not select database"); 
+			if(is_null($id)) {
+				$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
+			} else {							
+				$rs = mysql_query("SELECT * FROM `Books` WHERE `ID`='$ID'"); 
+			}
+		$row = mysql_fetch_array($rs);
+	
+		switch($Type) {
+			case "JSON":
+				$array = array('Author' => $row['Author'], 'Title' => $row['Title'], 'Content' => $row['Content']);
+				echo json_encode($array);		
+			break;
+			case "TEXT":			
+				echo "!Author-" .$row['Author']. "<br />";
+				echo "!Title-" .$row['Title']. "<br />";
+				echo $row['Content'];
+			break;
+			case "YAML":
+				echo "Author: \"" .$row['Author']."\"<br />";
+				echo "Title: \"" .$row['Title']. "\"<br />";
+				echo "Content: \"".$row['Content']."\"<br />";
+			default:	
+				echo "You queried an ID but didn't specify the type!";		
+			break;
+		}
 } else {
-	//ID is present, check for file type
 	if(is_null($Type)) {
 		die("Query is missing the type");
-	} else {
-		doDbWork();
-	}
-}
-mysql_close($link);
-
-function doDbWork($link) {
-		//Copy vars to avoid passing them?
-		$Author=$_GET["author"];
-		$Title=$_GET["title"];
-		$Type=$_GET["type"];
-		$ID=$_GET['id'];
-		$Ip = $_SERVER['REMOTE_ADDR'];
-		file_put_contents("logs/api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip : Querying '$Author - $Title'", FILE_APPEND | LOCK_EX);
+	} else {		
 		mysql_select_db($dbName) or die("Could not select database"); 
 			if(is_null($id)) {
 				$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
@@ -61,5 +71,8 @@ function doDbWork($link) {
 				echo "You queried an ID but didn't specify the type!";		
 			break;
 		}
+	}
 }
+file_put_contents("logs/api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip : Querying '$Author - $Title'", FILE_APPEND | LOCK_EX);
+mysql_close($link);
 ?> 
