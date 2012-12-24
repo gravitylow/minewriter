02682a0693dec7a300f5c304ca89d316b3f62a5e
Nick Porillo <milkywayz@mail.com>
2012-12-23 22:43:35 -0500

    Update homepage with more info

--- index.php
+++ index.php
@@ -18,30 +18,29 @@
               	<h2 class="big">What is MineWriter?</h2>     
 					<ul>
 						<li class="big">MineWriter is a service where you can write books for Minecraft!</li>
-						<li class="big">This site will be used to generate special text files which can be easily imported into Minecraft.</li>            
-						<li class="big">In order to port the books you write into minecraft, you need the BookDownloader plugin.</li>
+						<li class="big">This site will be used to generate JSON files which can be imported into Minecraft.</li>            
+						<li class="big">In order to port the books you write into minecraft, you need the MineReader plugin.</li>
 					</ul>   
 				<h2 class="big">Why use MineWriter</h2>     
 					<ul>
 						<li class="big">You can write extremely large books very easily.</li>
-						<li class="big">Your book is saved and you're given a permanent link; your work will never be lost!</li>            
-						<li class="big">You can make unlimited copies of your book.</li>
+						<li class="big">Your book is saved into our database so it is never lost!</li>            
+						<li class="big">You can make unlimited copies of your book in game.</li>
 						<li class="big">You can make revisions after it's 'published'.</li>
 					</ul>     
 				<h2 class="big">When will it launch?</h2>     
 					<ul>
-						<li class="big">As you can tell, the web development is in it's very early stages.</li>
-						<li class="big">The bukkit plugin is also still in development, but it's functional.</li>            
-						<li class="big">If we get contributors, it will greatly speed up the process.</li>
-						<li class="big">Stay in touch for future updates!</li>
+						<li class="big">Our Estimate is around late January, but this is not for sure.</li>          
+						<li class="big">More contributors could speed up the process greatly!</li>
+						<li class="big">Check out our <a href="https://github.com/milkywayz/minewriter">github repository!</a></li>
 					</ul>              
                </div>
               <div>
-              	<h2 class="big">What is BookDownloader?</h2>     
+              	<h2 class="big">What is MineReader?</h2>     
 					<ul>
-						<li class="big">BookDownloader is the Bukkit plugin that can port website content into books.</li>
-						<li class="big">While the plugin itself can parse any document, it defaults to only those from this site.</li>            
-						<li class="big">In order to port into Minecraft books, you need this plugin.</li>
+						<li class="big">MineReader is the Bukkit plugin that can port website content into books.</li>
+						<li class="big">The plugin uses this sites API service to generate your book!</li>            
+						<li class="big">If you want your book you write here in game, you need it!</li>
 					</ul>    
               </div>
             </div>
