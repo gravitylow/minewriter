0def9287c73c4ba46ddd82b25b7c67c2d6e6d79b
lol768 <support@technostripe.com>
2012-12-24 18:32:48 +0000

    Privacy

--- policy.php
+++ policy.php
@@ -15,7 +15,31 @@
 		<div class="page-header">
 		  <h1>Policy <small>Private Policy</small></h1>
 		</div>		   
-	  		  Policy here
+	  		  <h3>Privacy Policy</h3>
+<b>Information collected</b>
+<p>We collect information from you when you register on our site, create a book, fill out a form or join or use our services.</p>
+<p>When registering on our site, as appropriate, you may be asked to enter a username and/or e-mail address. Much of this information can be publicly hidden and will be kept secure. In addition, as there is no requirement to register, You may visit our site anonymously.</p>
+<b>Information use</b>
+<p>Any of the information we collect from you may be used in one of the following ways:</p>
+<ul>
+<br>
+<b>Information protection</b>
+<p>Passwords and other sensitive pieces of information are stored in a protected database and transformed with a salted cryptographic hash function which is irreversible and ensures data can not be retrieved.</p>
+<b>Cookies</b>
+<p>We use cookies (cookies are small text files that a site stores on your computers via your Web browser, if you allow) that enables the site to recognize your browser and recall selected information. Cookies will only be stored when you register/login, or use the community forums.</p>
+<p>We use cookies to understand and save your preferences and to retain login information (such as your username).</p>
+<b>Third party information disclosure</b>
+<p>We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. We may also release your information when we believe release is necessary to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. You agree that while using our services, we may advertise the fact by displaying non-sensitive information (such as your recent books, username etc) on the site.</p>
+<b>Third party links</b>
+<p>Occasionally, at our discretion, we may include links to other sites throughout the MineWriter website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. If you believe there is an issue with one of these third-party sites, use the contact form to let us know and we will attempt to recitfy the issue.</p>
+<b>California Online Privacy Protection Act Compliance</b>
+<p>We have taken the necessary precautions to be in compliance with the California Online Privacy Protection Act. We will not distribute personal information to outside parties without your consent.</p>
+<b>Childrens Online Privacy Protection Act Compliance</b>
+<p>We are in compliance with the requirements of COPPA (Childrens Online Privacy Protection Act), we do not collect any information from anyone under 13 years of age. Our website, products and services are all directed to people who are at least 13 years old or older. Warnings and other steps such as DOB verification (analysing the user's date of birth to ensure a high enough age) have been implemented to stop users who are too young from signing up to our service(s).</p>
+<b>Your Consent</b>
+<p>By using our site, you consent to our web site privacy policy.</p>
+<b>Changes to our Privacy Policy</b>
+<p>We may change this policy at any time with or without notice.</p>
 	  </div>
 	  	<?php footer(); ?>
 
