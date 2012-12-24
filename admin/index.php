27a5a6d82725b289f5339405af27406f62b242ea
gomeow <davismariotti@gmail.com>
2012-12-23 20:02:28 -0800

    Added Admin Page PlaceHolder

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<?php require("functions.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+	<?php headIncludes(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div class="container">
+
+      <div class="content">
+        <div class="page-header">
+          <h1>Admins <small>Don't abuse it!</small></h1>
+        </div> 
+        	Admin Page placeholder.     
+            </div>
+      </div>
+  </body>
+</html>
