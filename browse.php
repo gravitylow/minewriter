c576d59f1702bab621af40999d03a3fff6ac8a71
gomeow <davismariotti@gmail.com>
2012-12-23 16:06:14 -0800

    Changed description.

--- browse.php
+++ browse.php
@@ -12,7 +12,7 @@
 
       <div class="content">
         <div class="page-header">
-          <h1>MineWriter <small>Author downloadable books for minecraft!</small></h1>
+          <h1>Browse <small>Look at other MineWriter books!</small></h1>
         </div> 
         	Browse page placeholder     
             </div>
