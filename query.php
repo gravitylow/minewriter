926f57cc73a83678078a75b484b5a22cb1568c65
Nick Porillo <milkywayz@mail.com>
2012-12-24 00:10:20 -0500

    Move logs, use POST, change log message

--- query.php
+++ query.php
@@ -1,16 +1,16 @@
 <?php include "../private/config.php";
 
-$Author=$_GET["author"];
-$Title=$_GET["title"];
-$Type=$_GET["type"];
+$Author=$_POST["author"];
+$Title=$_POST["title"];
+$Type=$_POST["type"];
 $Ip = $_SERVER['REMOTE_ADDR'];
 
 
-$link = mysql_connect($dbHost, $dbUser, $dbPass) or die('Mysql error'); 
+$link = mysql_connect($dbHost, $dbUser, $dbPass) or die('Connection to MySQL has failed!'); 
 if (empty($_GET)) {
 	die('Invalid request');
 }
-	file_put_contents("api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip for $Author, $Title", FILE_APPEND | LOCK_EX);
+	file_put_contents("logs/api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip : Querying '$Author - $Title'", FILE_APPEND | LOCK_EX);
 	mysql_select_db($dbName) or die("Could not select database"); 
 	$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
 	$row = mysql_fetch_array($rs);
@@ -30,6 +30,7 @@ switch($Type) {
 			echo "Title: \"" .$row['Title']. "\"<br />";
 			echo "Content: \"".$row['Content']."\"<br />";
 	default:
+			
 		break;
 }
 mysql_close($link);
