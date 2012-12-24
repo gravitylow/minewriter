1e891cdb2d4f0270573f02bb1077b2b400f55b48
gomeow <davismariotti@gmail.com>
2012-12-23 19:48:35 -0800

    Removed spacing

--- contact.php
+++ contact.php
@@ -33,6 +33,3 @@
         <script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
         <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>        
         <script type="text/javascript" src="js/demo.js"></script>
-
-  </body>
-</html>
