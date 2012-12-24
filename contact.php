aa6f4d73a7e31928f325f447b4d8e5305f0c026f
Davis <Davis@Debs-MacBook-Pro.local>
2012-12-23 17:26:16 -0800

    Added Mail Script

--- contact.php
+++ contact.php
@@ -20,12 +20,12 @@
 						<li class="big">Fill out the form below and push submit!</li>
 					</ul>   
 					<div id="contactTable">
-               			<form id="contact">
+               			<form id="contact" action="contactMail()">
                				<table id="contactTable">
                				<br />
                					<tr><td class="left">UserName:</td><td><input class="contactInputs" type="text" name="name"/></td></tr>
                					<tr><td class="left">Email:</td><td><input class="contactInputs" type="text" name="email"/></td></tr>
-               					<tr><td class="left">Comments:</td><td><textarea class="contactInputs" cols="100" style="height: 70px;" id="contactText"></textarea></td></tr>
+               					<tr><td class="left">Comments:</td><td><textarea class="contactInputs" cols="100" style="height: 70px;" id="contactText" name="content"></textarea></td></tr>
                				</table>
                				<input type="submit" style="margin-left:180px;"/>
                			</form>
--- functions.php
+++ functions.php
@@ -47,4 +47,50 @@ function headIncludes() {?>
 <link href="//netdna.bootstrapcdn.com/font-awesome/2.0/css/font-awesome-more.css" rel="stylesheet">
 <link href="css/def.css" rel="stylesheet">
 <?php }
+
+
+function hasHtml($str){
+  if(strlen($str) != strlen(strip_tags($str)))
+      return true;  
+  return false;
+}
+
+
+
+function contactMail() {
+	$subject = "MineWriter Contact Results";
+	$to = "team@minewriter.net";
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	if(!preg_match('/^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/',$_POST['email'])) {
+		?><script type="text/javascript">
+			window.alert("Invalid email proved, the email must be in valid email format (such as name@domain.com).");
+		</script><?php
+		return;
+	}
+	if(!preg_match('/^[-_ 0-9a-z]$/i',$_POST['name'])) {
+		?><script type="text/javascript">
+			window.alert("Invalid name, the name may only contain a-z, A-Z, 0-9, "-", "_" and spaces.");
+		</script><?php
+		return;
+	}
+	$content = $_POST['content'];
+	if(hasHtml($content)) {
+		?><script type="text/javascript">
+			window.alert("You cannot use Html in this!");
+		</script><?php
+		return;
+	}
+
+	$message = <<<EOT
+	Name: $name
+	Email: $email
+	
+	Content: $content
+	EOT;
+	mail($to,
+		$subject,
+		$message);
+}
 ?>
