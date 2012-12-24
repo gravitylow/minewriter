2a46e2a9d8088d00e8543c04b28101f0df0b1ef6
Nick Porillo <milkywayz@mail.com>
2012-12-24 02:28:03 -0500

    Attempt functioned connections

--- query.php
+++ query.php
@@ -8,6 +8,7 @@ $Ip = $_SERVER['REMOTE_ADDR'];
 
 $link = mysql_connect($dbHost, $dbUser, $dbPass) or die('Connection to MySQL has failed!'); 
 if(is_null($id)) {
+	//No ID, lets check if theres anything else
 	if(is_null($Author)) {
 		die("Query is missing the author");
 	} else if(is_null($Title)) {
@@ -15,17 +16,25 @@ if(is_null($id)) {
 	} else if(is_null($Type)) {
 		die("Query is missing the type");
 	}	
+	doDbWork(, $id);
 } else {
+	//ID is present, check for file type
 	if(is_null($Type)) {
 		die("Query is missing the type");
 	} else {
+		doDbWork(, $id);
+	}
+}
+mysql_close($link);
+
+function doDbWork($link, $id) {
 		file_put_contents("logs/api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip : Querying '$Author - $Title'", FILE_APPEND | LOCK_EX);
 		mysql_select_db($dbName) or die("Could not select database"); 
-		if(is_null($ID)) {
-			$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
-		} else {
+			if(is_null($id)) {
+				$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
+			} else {							
 				$rs = mysql_query("SELECT * FROM `Books` WHERE `ID`='$ID'"); 
-		}	
+			}
 		$row = mysql_fetch_array($rs);
 	
 		switch($Type) {
@@ -46,7 +55,5 @@ if(is_null($id)) {
 				echo "You queried an ID but didn't specify the type!";		
 			break;
 		}
-	}
 }
-mysql_close($link);
 ?> 
