03ca4660d027ac39ea2c3e3210c576e0abfdd005
gomeow <davismariotti@gmail.com>
2012-12-23 20:17:39 -0800

    Added function for subfolders meta

--- functions.php
+++ functions.php
@@ -48,4 +48,11 @@ function headIncludes() {?>
 <link href="css/def.css" rel="stylesheet">
 <?php }
 
+function headIncludesSubFolders() {?>
+<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+<link href="//netdna.bootstrapcdn.com/bootswatch/2.1.1/cerulean/bootstrap.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/2.0/css/font-awesome-more.css" rel="stylesheet">
+<link href="../css/def.css" rel="stylesheet">
+<?php }
+
 ?>
