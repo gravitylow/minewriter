cb58c73c5cea980fd4b033ca1af0f2f3bc773a1f
Nick Porillo <milkywayz@mail.com>
2012-12-24 02:19:07 -0500

    Large fix to query

--- query.php
+++ query.php
@@ -7,39 +7,46 @@ $ID=$_GET['id'];
 $Ip = $_SERVER['REMOTE_ADDR'];
 
 $link = mysql_connect($dbHost, $dbUser, $dbPass) or die('Connection to MySQL has failed!'); 
-
-if(is_null($Author)) {
-	die("Query is missing the author");
-} else if(is_null($Title)) {
-	die("Query is missing the title");
-} else if(is_null($Type)) {
-	die("Query is missing the type");
-}
-	file_put_contents("logs/api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip : Querying '$Author - $Title'", FILE_APPEND | LOCK_EX);
-	mysql_select_db($dbName) or die("Could not select database"); 
-	if(is_null($ID)) {
-		$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
-	} else {
-		$rs = mysql_query("SELECT * FROM `Books` WHERE `ID`='$ID'"); 
+if(is_null($id)) {
+	if(is_null($Author)) {
+		die("Query is missing the author");
+	} else if(is_null($Title)) {
+		die("Query is missing the title");
+	} else if(is_null($Type)) {
+		die("Query is missing the type");
 	}	
-	$row = mysql_fetch_array($rs);
+} else {
+	if(is_null($Type)) {
+		die("Query is missing the type");
+	} else {
+		file_put_contents("logs/api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $Ip : Querying '$Author - $Title'", FILE_APPEND | LOCK_EX);
+		mysql_select_db($dbName) or die("Could not select database"); 
+		if(is_null($ID)) {
+			$rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
+		} else {
+				$rs = mysql_query("SELECT * FROM `Books` WHERE `ID`='$ID'"); 
+		}	
+		$row = mysql_fetch_array($rs);
 	
-switch($Type) {
-	case "JSON":
-			$array = array('Author' => $row['Author'], 'Title' => $row['Title'], 'Content' => $row['Content']);
-			echo json_encode($array);		
-		break;
-	case "TEXT":			
-			echo "!Author-" .$row['Author']. "<br />";
-			echo "!Title-" .$row['Title']. "<br />";
-			echo $row['Content'];
-		break;
-	case "YAML":
-			echo "Author: \"" .$row['Author']."\"<br />";
-			echo "Title: \"" .$row['Title']. "\"<br />";
-			echo "Content: \"".$row['Content']."\"<br />";
-	default:			
-		break;
+		switch($Type) {
+			case "JSON":
+				$array = array('Author' => $row['Author'], 'Title' => $row['Title'], 'Content' => $row['Content']);
+				echo json_encode($array);		
+			break;
+			case "TEXT":			
+				echo "!Author-" .$row['Author']. "<br />";
+				echo "!Title-" .$row['Title']. "<br />";
+				echo $row['Content'];
+			break;
+			case "YAML":
+				echo "Author: \"" .$row['Author']."\"<br />";
+				echo "Title: \"" .$row['Title']. "\"<br />";
+				echo "Content: \"".$row['Content']."\"<br />";
+			default:	
+				echo "You queried an ID but didn't specify the type!";		
+			break;
+		}
+	}
 }
 mysql_close($link);
 ?> 
