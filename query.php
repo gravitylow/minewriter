f3e1c2823fa53e50eacd6f8fefddf07664d47d0c
Nick Porillo <milkywayz@mail.com>
2012-12-23 21:15:01 -0500

    Log API requests

--- query.php
+++ query.php
@@ -2,18 +2,16 @@
 
 $Author=$_GET["author"];
 $Title =$_GET["title"];
+$ip = $_SERVER['REMOTE_ADDR'];
 //Don't expose any DB details
-$link = mysql_connect("localhost", $dbUser, $dbPass); 
+$link = mysql_connect($dbHost, $dbUser, $dbPass); 
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
-echo 'Connected successfully';
 mysql_select_db($dbName) or die("Could not select database"); 
-
 $arr = array(); 
-//Make this PRECISE!
+file_put_contents("api_requests.txt", "\n" . date("Y-m-d H:i:s") . ": Recieved API request from $ip for $Author, $Title", FILE_APPEND | LOCK_EX);
 $rs = mysql_query("SELECT * FROM `Books` WHERE `Author`='$Author' AND `Title`='$Title' LIMIT 1"); 
-
 $rows = array();
 while($r = mysql_fetch_assoc($rs)) {
     $rows[] = $r;
