7b84ac90409218b19944ddc26734e8acbf914a9f
lol768 <support@technostripe.com>
2012-12-28 16:28:44 +0000

    Fix nsfw

--- read.php
+++ read.php
@@ -70,13 +70,13 @@ $nsfw = $row['nsfw'];
           	<br />
           	<strong>Created: <?php echo $date; ?></strong>
 	<br /><br />
-	<?php if ($nswf == '1') { ?><center><a href="#" role="button" class="btn btn-danger" onclick = '$("#writing").show()'><i class = "icon-exclamation-sign"></i> Show adult content</a></center><br /><?php } ?>
+	<?php if ($nsfw == '1') { ?><center><a href="#" role="button" class="btn btn-danger" onclick = '$("#writing").show()'><i class = "icon-exclamation-sign"></i> Show adult content</a></center><br /><?php } ?>
 	<center><a href="#myModal" role="button" class="btn btn-danger" data-toggle="modal"><i class = "icon-flag"></i> Report content</a></center>
           
           </div>
           
          
-       <textarea class = "book" id = "writing" name = "bookContent" style = "cursor: default !important; <?php if ($nsfw == '1') { ?>visibility: none;<?php } ?>" readonly><?php echo ($content); ?></textarea>
+       <textarea class = "book" id = "writing" name = "bookContent" style = "cursor: default !important; <?php if ($nsfw == '1') { ?>visibility: hidden;<?php } ?>" readonly><?php echo ($content); ?></textarea>
        
           <br /> 
         </div>
