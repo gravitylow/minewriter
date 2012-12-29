060949ff5b86918daeb75264b04db2e6db3317fc
Nick Porillo <milkywayz@mail.com>
2012-12-29 14:44:24 -0500

    Is medium a valid button size? I hope ;P

--- index.php
+++ index.php
@@ -21,17 +21,17 @@
 		<div class="span4">  
 			<h2 class="small">Server Owners</h2>  
 				<p>We want to make it as easy as possible for your players to author books and get them onto your server. Using the Bukkit plugin called MineReader, your players can easily get any book they want in game, with ease of use!</p>  
-				<p><a class="btn btn-success btn-large" href="#">Get the plugin</a></p>  
+				<p><a class="btn btn-success btn-medium" href="#">Get the plugin</a></p>  
 		</div>  
 		<div class="span4">  
 			<h2 class="small">Regular Players</h2>  
 				<p>Writing books and bringing them onto your favorite server is now easier then ever!</p>  
-				<p><a class="btn btn-success btn-large" href="#">See more</a></p>  
+				<p><a class="btn btn-success btn-medium" href="#">See more</a></p>  
 		</div>  
 		<div class="span4">  
 			<h2 class="small">Developers</h2>  
 				<p>All developers are invited to help us improve our website and minereader plugin, both are open sourced and we welcome all contributors</p>  
-				<p><a class="btn btn-success btn-large" href="devs.php">Visit the dev page</a></p>  
+				<p><a class="btn btn-success btn-medium" href="devs.php">Visit the dev page</a></p>  
 		</div> 
 		</div>
     </div>
