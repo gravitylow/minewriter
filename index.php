8c956b3404f4343706af4692e08f344232cf01f4
Nick Porillo <milkywayz@mail.com>
2012-12-29 14:37:45 -0500

    Lets get row aligned hopefully

--- index.php
+++ index.php
@@ -17,25 +17,26 @@
 		<p><a href="write.php" class="btn btn-success btn-large">Try now</a></p>  
 		</div>  
 		<div class="row">  
+		
 		<div class="span4">  
 			<h2>Server Owners</h2>  
 				<p>We want to make it as easy as possible for your players to author books and get them onto your server. Using the Bukkit plugin called MineReader, your players can easily get any book they want in game, with ease of use!</p>  
 				<p><a class="btn btn-success btn-large" href="#">Get the plugin</a></p>  
-			</div>  
-			<div class="span4">  
+		</div>  
+		<div class="span4">  
 			<h2>Regular Players</h2>  
 				<p>Writing books and bringing them onto your favorite server is now easier then ever!</p>  
 				<p><a class="btn btn-success btn-large" href="#">See more</a></p>  
-			</div>  
-			<div class="span4">  
+		</div>  
+		<div class="span4">  
 			<h2>Developers</h2>  
 				<p>All developers are invited to help us improve our website and minereader plugin, both are open sourced and we welcome all contributors</p>  
 				<p><a class="btn btn-success btn-large" href="devs.php">Visit the dev page</a></p>  
-			</div> 
+		</div> 
 		</div>
-    </div> 
     </div>
-    <?php footer(); ?>
-    </div>      
-</body>
+    </div>
+       <?php footer(); ?>    
+    </div>
+  </body>
 </html>
