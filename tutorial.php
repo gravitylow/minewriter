f4a421c70e233b6cfa8a5a92e143744347c36c13
Nick Porillo <milkywayz@mail.com>
2012-12-29 15:46:34 -0500

    New Tut page, only a placeholder for now, but will soon be helpful

---
+++ tutorial.php
@@ -0,0 +1,24 @@
+<?php require("functions.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MineWriter Tutorial</title>
+	<?php headIncludes(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div id = "wrap">
+    <div class="container">
+
+      <div class="content">
+        <div class="page-header">
+          <h1>Tutorial <small>Learn how to use MineWriter / MineReader</small></h1>
+        </div>
+        <p> This is a placeholder, this page will be filled with glorious info soon! :)</p>            
+      </div>
+    </div>
+    </div>
+    <?php footer(); ?>
+  </body>
+</html>
