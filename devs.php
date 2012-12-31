04176cddfa22fdfe5bb87487617aa389214fe568
lol768 <support@technostripe.com>
2012-12-31 15:06:45 +0000

    Spacing

--- devs.php
+++ devs.php
@@ -36,9 +36,9 @@
 						<li class="big">Example Query: http://minewriter.net/query.php?id=5&type=YAML</li>
 					</ul>              
                </div>
-               <h2 class="big">Links</h2>     
+               <h2 class="big" style = "list-style: none;">Links</h2>     
 					<ul>
-						<li class="big"><a class="btn btn-success btn-large" href="http://goo.gl/sRR9u">MineWriter Github</a></li>         
+						<li class="big spaced"><a class="btn btn-success btn-large" href="http://goo.gl/sRR9u">MineWriter Github</a></li>         
 						<li class="big"><a class="btn btn-success btn-large" href="http://goo.gl/cgj2T">MineReader Github</a></li> 
 						<li class="big"><a class="btn btn-success btn-large" href="">Documentation</a></li> 
 					</ul>              
