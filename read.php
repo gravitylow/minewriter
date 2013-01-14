e14cb49e516856ce04c8e3092b8ce9ba8f32db32
gomeow <davismariotti@gmail.com>
2013-01-14 13:03:26 -0800

    Even more prettiness

--- read.php
+++ read.php
@@ -92,8 +92,8 @@ if ($downloads == "") {
             <div class = "well" style = "position: absolute; right: 0; top: 260px; width: 220px;">
           <?php topHeader("Staff tools"); ?>
           <form method="POST" action="someForm.php">
-          	<center><button type="button" class="btn btn-warning" style="margin-bottom: 10px;"><i class = "icon-minus-sign"></i> Hide Book</button><br />
-          	<button type="button" class="btn btn-danger" width="130"><i class = "icon-trash"></i> Delete Book</button><br /></center>
+          	<center><button type="button" class="btn btn-warning" style="margin-bottom: 10px;width:130px;"><i class = "icon-minus-sign"></i> Hide Book</button><br />
+          	<button type="button" class="btn btn-danger" style=" width:130px;"><i class = "icon-trash"></i> Delete Book</button><br /></center>
           </form>
           </div>
           
