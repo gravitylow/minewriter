87e6fce4bb599b4864b221165ad4f3a9dd7c81ff
lol768 <support@technostripe.com>
2013-01-14 18:38:06 +0000

    More error messages

--- login.php
+++ login.php
@@ -22,6 +22,9 @@
 		    </div>
 		    <?php if ($_GET['e'] == 1) {?><div class = "alert-error alert"><i class = "icon-lock"></i> Incorrect username or password.</div><?php } ?>
 		    <?php if ($_GET['e'] == 2) {?><div class = "alert-error alert"><i class = "icon-ban-circle"></i> You have been banned: <?php echo (strip_tags(base64_decode($_GET['reason']))); ?></div><?php } ?>
+		    <?php if ($_GET['e'] == 3) {?><div class = "alert-error alert"><i class = "icon-envelope"></i> Please verify your email address before logging in.</div><?php } ?>
+		    <?php if ($_GET['e'] == 4) {?><div class = "alert-success alert"><i class = "icon-check"></i> Your email address has been verified. You may now login.</div><?php } ?>
+		     <?php if ($_GET['e'] == 4) {?><div class = "alert-success alert"><i class = "icon-signout"></i> You have been successfully logged out.</div><?php } ?>
 		    <div class="control-group">
 		      <label class="control-label" for="username">Username</label>
 		      <div class="controls">
