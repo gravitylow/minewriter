f8bb8fcdf887fd5d4413033d8cec7ca70f04127c
gomeow <davismariotti@gmail.com>
2013-01-16 14:24:24 -0800

    Start basic register script

--- registerScript.php
+++ registerScript.php
@@ -18,4 +18,53 @@ die ($db->getMessage());
 }
 $username = strtolower($_POST['username']);
 $password = $_POST['password'];
+$confirm = $_POST['password_confirm'];
+$email = $_POST['email'];
+
+
+if($password !== $confirm) {
+	header("Location: login.php?e=7");
+	die();
+}
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	header("Location: login.php?e=8");
+	die();
+}
+
+$db = connectDB($dbUser, $dbPass, $dbName);
+if ($db instanceof PDOException) {
+	die ($db->getMessage());
+}
+$query = "SELECT count(*) FROM `Books` WHERE username = :username";
+$stmt->bindParam(':username', $username);
+$stmt = $db->prepare($query);
+$stmt->execute();
+$row = $stmt->fetch();
+foreach($row as $value) {
+	if($value != 0) {
+		//Username taken
+		header("Location: login.php?e=10");
+		die();
+	}
+	break;
+}
+$query = "SELECT count(*) FROM `Books` WHERE email = :email";
+$stmt->bindParam(':email', $email);
+$stmt = $db->prepare($query);
+$stmt->execute();
+$row = $stmt->fetch();
+foreach($row as $value) {
+	if($value != 0) {
+		//Email taken
+		header("Location: login.php?e=9");
+		die();
+	}
+	break;
+}
+$hash = secureHash($username, $password);
+//Insert add to table here.
+
+
+header("Location: login.php?e=11");
+die();
 ?>
