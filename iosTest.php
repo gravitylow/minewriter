1e66339fde68eb3492efc0f175ac7c0abb8eea0e
lol768 <support@technostripe.com>
2013-01-16 18:29:55 +0000

    Fix ;

--- iosTest.php
+++ iosTest.php
@@ -4,11 +4,11 @@ $detect = new Mobile_Detect();
 if($detect->isiOS()){
     echo("iOS has been detected<br>");
     if($detect->isIpad()) {
-        echo("It's an iPad, running version: " . $detect->version('iPad'););
+        echo("It's an iPad, running version: " . $detect->version('iPad'));
     }
 
   if($detect->isIphone()) {
-    echo("It's an iPhone, running version: " . $detect->version('iPhone'););
+    echo("It's an iPhone, running version: " . $detect->version('iPhone'));
   }
 } else {
     die("No iOS detected.");
