cedda3cce8c2400d1945bd26a215d7110a433a9c
Nick Porillo <milkywayz@mail.com>
2013-01-17 18:39:26 -0500

    Add new file for getting difference in book took

---
+++ diff.php
@@ -0,0 +1,21 @@
+<?php 
+require("functions.php"); 
+require("../private/config.php");
+
+function connectDB($user, $pass, $db) {
+	try {	
+		return(new PDO("mysql:host=localhost;dbname=" . $db . ";charset=utf8", $user, $pass));
+	} catch(PDOException $ex) {
+		die($user);
+		return $ex;
+	}
+}
+
+$has=$_GET["has"];
+if(isset($has)) {
+	die("0");
+} else {
+	$total = $db->lastinsertid();
+	echo($total - $has);
+}
+?>
