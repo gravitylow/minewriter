40ae06782d5d1c762d6d938e6b634b3cc3911886
Nick Porillo <milkywayz@mail.com>
2013-01-18 15:15:55 -0500

    Sanitize and decode

--- stat.php
+++ stat.php
@@ -2,6 +2,8 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $input = file_get_contents('php://input');
+  $input = substr($input, 5);
+  $input = urldecode($input);
   $jsonObj = json_decode($input, true);
   file_put_contents("logs/stats.txt", "\n" . date("Y-m-d H:i:s") . ": Updated stats to: $jsonObj", FILE_APPEND | LOCK_EX);
   }
