e39e47fd6c3e9b2c91ad24f2fa0dcc000c4cd90e
Nick Porillo <milkywayz@mail.com>
2013-01-18 15:52:13 -0500

    The crudest states page, based from index.html

---
+++ stats.php
@@ -0,0 +1,47 @@
+<?php
+require("functions.php"); 
+require("../private/config.php");
+
+function connectDB($user, $pass, $db) {
+	try {	
+		return(new PDO("mysql:host=localhost;dbname=" . $db . ";charset=utf8", $user, $pass));
+	} catch(PDOException $ex) {
+		return $ex;
+	}
+	
+}
+
+
+$db = connectDB($dbUser, $dbPass, $dbName);
+if ($db instanceof PDOException) {
+	die ($db->getMessage());
+}
+$query = "SELECT * FROM `Stats` WHERE 1";
+$stmt = $db->prepare($query);
+$stmt->execute();
+$row = $stmt->fetch();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>MineWriter Metrics</title>
+	<?php headIncludes(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div id = "wrap">
+    <div class="container">   
+    <div class="content">
+    	<p>Crude stats page, this will be improved</p>
+    	<? while($showtablerow = $stmt->fetch()) {
+	    	echo $showtablerow[0]."<br />"; }?>			
+        <?php divider(); ?>        
+    </div>
+    </div>
+       <?php footer(); ?>    
+    </div>
+  </body>
+</html>
