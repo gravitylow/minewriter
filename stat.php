76bae3ec67e7058e407c609266b52b5c44f9dbdc
Nick Porillo <milkywayz@mail.com>
2013-01-18 15:47:19 -0500

    Use Auth to prevent others to update stats, and use the DB

--- stat.php
+++ stat.php
@@ -1,13 +1,38 @@
 <?php
+require("../private/config.php");
+$ip = $_SERVER['REMOTE_ADDR'];
+$auth = false;
+error_reporting(E_ERROR);
+if ($ip == "74.106.16.4") {
+	$auth = true;
+}
+if (!$auth) {
+	file_put_contents("logs/stats.txt", "\n" . date("Y-m-d H:i:s") . ": Unauthorised stats update attempt from $ip", FILE_APPEND | LOCK_EX);
+	die("Not authorised.");
+}
+function connectDB($user, $pass, $db) {
+try {	
+	return(new PDO("mysql:host=localhost;dbname=" . $db . ";charset=utf8", $user, $pass));
+} catch(PDOException $ex) {
+	return $ex;
+}
+
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $input = file_get_contents('php://input');
-  $input = substr($input, 5);
-  $input = urldecode($input);
-  $jsonObj = json_decode($input, true);
-  foreach ($jsonObj as $name => $value) {
-  	file_put_contents("logs/stats.txt", "\n" . $name . ": $value", FILE_APPEND | LOCK_EX);
-  }  
-  }
+	$db = connectDB($dbUser, $dbPass, $dbName);
+	if ($db instanceof PDOException) {
+		die ($db->getMessage());
+		}
+	$input = file_get_contents('php://input');
+	$input = substr($input, 5);
+	$input = urldecode($input);
+	$jsonObj = json_decode($input, true);
+	$sql = "UPDATE `Stats` SET `TotalChars`=Chars,`AverageChars`=AvgChars,`BookCount`=Count,`MostUsedWord`=FavWord,`LongestBook`=LongestBook WHERE 1";
+	$stmt = $db->prepare($sql);
+	foreach ($jsonObj as $name => $value) {
+			$stmt->bindParam($name,$value);
+		} 
+		$stmt->execute(); 
+	}
 ?>
-  
