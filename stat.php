c8eb751d219cd578224af08604cb4b014279045c
Nick Porillo <milkywayz@mail.com>
2013-01-18 15:21:37 -0500

    Loop the json array and log the text

--- stat.php
+++ stat.php
@@ -5,7 +5,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $input = substr($input, 5);
   $input = urldecode($input);
   $jsonObj = json_decode($input, true);
-  file_put_contents("logs/stats.txt", "\n" . date("Y-m-d H:i:s") . ": var_dump($jsonObj)", FILE_APPEND | LOCK_EX);
+  foreach ($jsonObj as $name => $value) {
+  	file_put_contents("logs/stats.txt", "\n" . $name . ": $value", FILE_APPEND | LOCK_EX);
+  }  
   }
 ?>
   
