6832a18283afa3924b8a7f8a97a6a3f9d7fb1f11
Nick Porillo <milkywayz@mail.com>
2013-01-18 15:18:21 -0500

    Vardump the json

--- stat.php
+++ stat.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $input = substr($input, 5);
   $input = urldecode($input);
   $jsonObj = json_decode($input, true);
-  file_put_contents("logs/stats.txt", "\n" . date("Y-m-d H:i:s") . ": Updated stats to: $jsonObj", FILE_APPEND | LOCK_EX);
+  file_put_contents("logs/stats.txt", "\n" . date("Y-m-d H:i:s") . ": var_dump($jsonObj)", FILE_APPEND | LOCK_EX);
   }
 ?>
   
