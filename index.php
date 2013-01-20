c25c997ada4ed271e4bf5e5bae7054a564006ba5
Nick Porillo <milkywayz@mail.com>
2013-01-19 21:07:04 -0500

    Cleanup + re-wording

--- index.php
+++ index.php
@@ -7,16 +7,14 @@ function connectDB($user, $pass, $db) {
 		return(new PDO("mysql:host=localhost;dbname=" . $db . ";charset=utf8", $user, $pass));
 	} catch(PDOException $ex) {
 		return $ex;
-	}
-	
+	}	
 }
-
-$booksAmt = 0;
 
 $db = connectDB($dbUser, $dbPass, $dbName);
 if ($db instanceof PDOException) {
 	die ($db->getMessage());
 }
+$booksAmt = 0;
 $query = "SELECT * FROM `Stats` WHERE 1";
 $stmt = $db->prepare($query);
 $stmt->execute();
@@ -38,14 +36,12 @@ $booksAmt = $row['BookCount'] -1;
     <div class="content">
 		<div class="leaderboard" style = "position: relative">  
 		<h1>MineWriter</h1>  
-		<p>Try out our user friendly authoring service!</p>
-		<p>Over <?php echo($booksAmt); ?> books written so far!</p>  
-		<p><a href="write.php" class="btn btn-primary">Try now!</a></p> 
-		<a class="thumbnail" href="#" style = "position: absolute; right: 25px; top: 30px;">
-                  <img alt="260x180" src="http://i.imgur.com/0RWRs.png" style="width: 438px; height: 192px;" >
-                </a>
-		</div>  
-				
+			<p>Try out our user friendly authoring service!</p>
+			<p>Over <?php echo($booksAmt); ?> books written so far!</p>  
+			<p><a href="write.php" class="btn btn-primary">Try now!</a></p> 
+			<a class="thumbnail" href="#" style = "position: absolute; right: 25px; top: 30px;">
+			<img alt="260x180" src="http://i.imgur.com/0RWRs.png" style="width: 438px; height: 192px;" ></a>
+		</div>  				
 		<div class="row">
 		  <div class="span9">
 		    <h2>About the project</h2>
@@ -57,19 +53,16 @@ $booksAmt = $row['BookCount'] -1;
 		      		<li><p>Both MineWriter and MineReader are open-source and we welcome contributors</p></li>
 		      	</ul>
 		      </div>
-		      <div class="span2">
-		      
+		      <div class="span2">		      
 		      </div>
 		    </div>
 		  </div>
 		</div>
-		
 		<?php divider(); ?>
-		
         <div class="row"> 
         	<div class="span4">  
 				<h2>Server Owners</h2>  
-					<p>We want to make it as easy as possible for your players to author books and get them onto your server. Using the Bukkit plugin called MineReader, your players can easily get any book they want in game, with ease of use!</p>  
+					<p>We want to make it as easy as possible for your players to author books and get them onto your server. Using the Bukkit plugin called MineReader, your players can get any book they want in game, it's that easy!</p>  
 					<p><a class="btn btn-primary" href="http://dev.bukkit.org/server-mods/minereader/">Get the plugin</a></p>  
 			</div>  
 			<div class="span4">  
@@ -79,12 +72,11 @@ $booksAmt = $row['BookCount'] -1;
 			</div>  
 			<div class="span4">  
 				<h2>Developers</h2>  
-					<p>All developers are invited to help us improve our website and minereader plugin, both are open sourced and we welcome all contributors</p>  
+					<p>Any developer is welcome to help us improve our website and minereader plugin, both are open sourced and on github</p>  
 					<br /><br /><p><a class="btn btn-primary" href="devs.php">Visit the API page</a></p>  
 			</div>	
         </div> 	
-        <?php divider(); ?>
-        
+        <?php divider(); ?>       
     </div>
     </div>
        <?php footer(); ?>    
