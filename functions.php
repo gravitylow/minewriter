b1e592cc9697ac58846b2ba8568ece61d13ba857
ftbastler <user@server.end>
2013-01-29 13:38:48 +0100

    Adding jQuery

--- functions.php
+++ functions.php
@@ -173,6 +173,7 @@ function footer() {
 	<div class="container" style = "position: relative; top: 20px;">
 	<p class="muted credit" style = "display: inline"><a href="tos.php">Terms of Service</a> | <a href="policy.php">Privacy Policy</a></p> <p class = "muted credit pull-right" style = "display: inline"><a href = "https://github.com/milkywayz/minewriter/">GitHub</a></p>
 </div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 <?php }
 
 ?>
