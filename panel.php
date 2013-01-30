b2a751dbd7f1344c3a24aa9953a253249cdf77dd
gomeow <davismariotti@gmail.com>
2013-01-30 07:20:27 -0800

    Create panel.php

---
+++ panel.php
@@ -0,0 +1,24 @@
+<?php require("functions.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Develop for MineWriter</title>
+	<?php headIncludes(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div id = "wrap">
+    <div class="container">
+
+      <div class="content">
+        <div class="page-header">
+          <h1>Panel <small>Manage your account!</small></h1>
+		This is just a place holder for the account panel.
+        </div>
+      </div>
+    </div>
+      	<?php footer(); ?>
+
+  </body>
+</html>
