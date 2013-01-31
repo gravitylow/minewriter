a307d7591e804329abf6652ceaafc46354a2311f
lol768 <support@technostripe.com>
2013-01-31 21:11:11 +0000

    Update js/pwstrength.js

--- js/pwstrength.js
+++ js/pwstrength.js
@@ -22,7 +22,7 @@
                 same_as_username: "Your password cannot be the same as your username"
             },
             scores: [17, 26, 40, 50],
-            verdicts: ["Weak", "Normal", "Medium", "Strong", "Excellent"],
+            verdicts: ["Weak", "Normal", "Medium", "Good", "Strong"],
             showVerdicts: true,
             raisePower: 1.4,
             usernameField: "#username",
